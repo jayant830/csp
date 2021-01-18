35af7d33f51cf1903faa15a3c2c495f5f15bf960
Jayant Dharwadkar <jayant.dharwadkar@gmail.com>
2021-01-18 14:47:23 -0800

    image addition

--- teachers.js
+++ teachers.js
@@ -10,8 +10,8 @@ document.addEventListener('DOMContentLoaded', () => {
             //document.getElementById('output').textContent = data;
             //console.log(xml);
             buildNameList(xml);
-            //buildImageList(xml);
             buildBlurbList(xml);
+            buildImageList(xml);
         });
 })
 
@@ -33,8 +33,17 @@ function buildBlurbList(x) {
     for (let i = 0; i < text.length; i++) {
         let li = document.createElement('li');
         let blurb = text[i].lastChild.nodeValue;
-        //let person = swords[i].getAttribute('owner');
         li.textContent = blurb;
+        list.appendChild(li);
+    }
+}
+function buildImageList(x) {
+    let list = document.getElementById('pic');
+    let pic = x.getElementsByTagName('image');
+    for (let i = 0; i < pic.length; i++) {
+        let li = document.createElement('li');
+        let image = pic[i].lastChild.nodeValue;
+        li.textContent = image;
         list.appendChild(li);
     }
 }
