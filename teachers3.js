14065cf8345f7a743b26ce707e6f4042ec5845fe
Mukul Dharwadkar <dharmuku@amazon.com>
2021-01-21 10:47:10 -0800

    New page with semantics

---
+++ teachers3.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //fetch the data as soon as the page has loaded
+    let url = "teachers.xml";
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            //console.log(data); //string
+            let parser = new DOMParser();
+            let xml = parser.parseFromString(data, "application/xml");
+            //document.getElementById('output').textContent = data;
+            //console.log(xml);
+            buildNameList(xml);
+            //buildImageList(xml);
+            buildBlurbList(xml);
+        });
+})
+
+function buildNameList(x) {
+    let list = document.getElementById('member');
+    let member = x.getElementsByTagName('name');
+    for (let i = 0; i < member.length; i++) {
+        let li = document.createElement('li');
+        li.classList.add("libox", "box-small", "neg-margin")
+            //li.classList.add("box-small")
+        let name = member[i].firstChild.nodeValue;
+        li.textContent = name;
+        list.appendChild(li);
+    }
+}
+
+function buildBlurbList(x) {
+    let list = document.getElementById('text');
+    let text = x.getElementsByTagName('blurb');
+    for (let i = 0; i < text.length; i++) {
+        let li = document.createElement('li');
+        //li.classList.add("box-large")
+        li.classList.add("libox", "box-large", "neg-margin")
+        let blurb = text[i].firstChild.nodeValue;
+        li.textContent = blurb;
+        list.appendChild(li);
+    }
+}
